465987d0575502ac6e29181fe9e4e0a3e33f5733
Marcos Faglioni <marcosfagli@hotmail.com>
2017-06-13 12:13:26 -0300

    Create Trabalhov1.asm

--- Trabalhov1.asm
+++ Trabalhov1.asm
@@ -5,11 +5,8 @@ INCLUDE Irvine32.inc
 	tempo BYTE "TEMPO:", 0				;Nome do marcador de Tempo 
 	pontuacao BYTE "PONTUACAO:", 0		;Nome do marcador de Pontuação
 	biniciar BYTE "INICIAR",0			;Nome do botão para iniciar o jogo
-	biniciari BYTE "Pressione w",0		;Nome da informação de como acessar o inicio do jogo
 	bcreditos BYTE "CREDITOS",0			;Nome do botão para os creditos
-	bcreditosi BYTE "Pressione a",0		;Nome da  informação de como acessar os créditos
 	bcomoJogar BYTE "COMO JOGAR",0		;Nome do botão para as instruções
-	bcomoJogari BYTE "Pressione s",0	;Nome da informação de como acessar as instruções
 	nome BYTE "CLOCK COLORS", 0			;Nome do Jogo
 	time BYTE 0							;Armazena o tempo do jogo
 	score BYTE 0						;Armazena a pontuação do jogo
@@ -81,13 +78,14 @@ LPS2:
 	
 	ret
 PrintSeta ENDP	
-	
-	
 
-main PROC
-	jmp start
 
-Bordas:
+Bordas PROC
+;Imprime uma seta na posição desejada
+;Recebe:	
+;
+;
+;Retorna:	
 	mov eax, red+(black*16)
 	call SETTEXTCOLOR
 	
@@ -123,8 +121,9 @@ L3:
 	call SETTEXTCOLOR
 	
 	ret
-
-Plataformas:
+Bordas ENDP	
+	
+Plataformas PROC
 	mov dl, 1								;Trecho para imprimir as 4 plataformas existentes no jogo (loop aninhados)
 	mov dh, 4
 	call GOTOXY
@@ -151,9 +150,9 @@ L4:
 	call SETTEXTCOLOR
 	
 	ret
-
+Plataformas ENDP
 	
-TelaInicio:
+TelaInicio PROC
 	mov eax, white+(black*16)
 	call SETTEXTCOLOR
 
@@ -168,32 +167,17 @@ TelaInicio:
 	call GOTOXY
 	mov edx, OFFSET biniciar
 	call WRITESTRING
-	mov dl, 15								;Trecho para imprimir a opção iniciar na tela principal
-	mov dh, 6
-	call GOTOXY
-	mov edx, OFFSET biniciari
-	call WRITESTRING
 	
 	mov dl, 16								;Trecho para imprimir a opção Como jogar na tela principal
 	mov dh, 8
 	call GOTOXY
 	mov edx, OFFSET bcomoJogar
 	call WRITESTRING
-	mov dl, 15								;Trecho para imprimir a opção Como jogar na tela principal
-	mov dh, 9
-	call GOTOXY
-	mov edx, OFFSET bcomoJogari
-	call WRITESTRING
 	
 	mov dl, 17								;Trecho para imprimir a opção creditos na tela principal
 	mov dh, 11
 	call GOTOXY
 	mov edx, OFFSET bcreditos
-	call WRITESTRING
-	mov dl, 15								;Trecho para imprimir a opção creditos na tela principal
-	mov dh, 12
-	call GOTOXY
-	mov edx, OFFSET bcreditosi
 	call WRITESTRING
 	
 	mov eax, white+(black*16)
@@ -204,9 +188,9 @@ TelaInicio:
 	call GOTOXY
 	
 	ret
-	
+TelaInicio ENDP
 
-TelaJogo:									
+TelaJogo PROC									
 	mov eax, red							;IRVINE red - Seleção de cores pré definidas no IRVINE
 	call SETTEXTCOLOR						;IRVINE SETTEXTCOLOR - Seta a cor do texto e a cor do fundo da fonte
 	mov dl, 9
@@ -223,9 +207,9 @@ TelaJogo:
 	mov eax, white+(black*16)
 	call SETTEXTCOLOR
 	ret
-	
+TelaJogo ENDP
 
-TelaIntrucoes:
+TelaIntrucoes PROC
 	mov eax, white+(black*16)
 	call SETTEXTCOLOR
 
@@ -234,8 +218,9 @@ TelaIntrucoes:
 	call GOTOXY
 	mov edx, OFFSET nome
 	call WRITESTRING
+TelaIntrucoes ENDP
 	
-TempoTela:
+TempoTela PROC
 	mov dl, 16
 	mov dh, 1
 	call GOTOXY
@@ -247,35 +232,44 @@ TempoTela:
 	mov dh, 16
 	call GOTOXY
 	ret
+TempoTela ENDP
 	
-	
-ScoreTela:
+ScoreTela PROC
 	mov dl, 32
 	mov dh, 1
 	call GOTOXY
 	movzx eax, score
 	call WRITEDEC
 	ret
+ScoreTela ENDP
+
+
 	
+main PROC
+
 start:
 	call CLRSCR								;IRVINE CLRSCR - Limpa a tela
 	call Bordas
 	call Plataformas
 	call TelaInicio
 	
-LookForKey:
+AguardaTecla1:
     mov  eax,50          					;Tempo para o SO esperar
     call Delay           					; (otherwise, some key presses are lost)
-
     call ReadKey         					;Busca por entradas no teclado
-    jz   LookForKey      					;Nenhuma tecla pressionada ainda
-
+    jz   AguardaTecla1      					;Nenhuma tecla pressionada ainda
+	cmp  dx,000Dh  							;Compara a entrada do teclado com "enter"
+	je LS1
 	call PrintSeta
-
-    cmp    dx,000Dh  						;Compara a entrada do teclado com "enter"
-    jne    LookForKey    					;Se a tecla não for enter, volta para o label LookForKey
+    jne  AguardaTecla1    					;Se a tecla não for enter, volta para o label AguardaTecla1
 	
-
+LS1:
+	cmp posSeta, 0000h
+	je jogo
+	cmp posSeta, 0001h
+	je instrucoes
+	cmp posSeta, 0002h
+	je creditos
 	
 jogo:
 	call CLRSCR	
@@ -288,55 +282,7 @@ jogo:
 	mov eax, 1000
 	call DELAY
 	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
 	jmp fim
-	
-prox1:	
-	cmp al, 's'
-	je instrucoes
-	cmp al, 'S'
-	jne prox2
 	
 instrucoes:
 	call CLRSCR	
@@ -346,12 +292,6 @@ instrucoes:
 	call READCHAR
 	jmp start
 	
-prox2:
-	cmp al, 'a'
-	je creditos
-	cmp al, 'A'
-	jne prox2
-
 creditos:
 	call CLRSCR	
 	call Bordas
