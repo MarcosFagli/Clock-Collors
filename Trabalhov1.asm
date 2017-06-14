b0e489e3f4994c6bf5a6019b4c91e94e4eac664f
Marcos Faglioni <marcosfagli@hotmail.com>
2017-06-14 08:27:28 -0300

    Update Trabalhov1.asm

--- Trabalhov1.asm
+++ Trabalhov1.asm
@@ -11,17 +11,61 @@ INCLUDE Irvine32.inc
 	time BYTE 0							;Armazena o tempo do jogo
 	score BYTE 0						;Armazena a pontuação do jogo
 	posSeta BYTE 0						;Armazena a posição da seta no menu 
+	tMaxX BYTE 60						;Armazena a quantidade de colunas do ecrã do jogo
+	tMaxY BYTE 26						;Armazena a quantidade de linhas do ecrã do jogo
+	distPlat BYTE 5						;Armazena a distancia entre as plataformas
+	platInicial WORD 8					;Armazena qual é a altura Y da plataforma mais alta
+	
+	;Os dados seguintes salvos na memória tem por objetivo armazenar o texto a ser exibido nas instrucões
+	mInstrucoes1 BYTE "ESTE JOGO CONSITE EM GUIAR O ETEVALDO ATE A",0
+	mInstrucoes2 BYTE "PLATAFORA MAIS ALTA QUE O JOGADOR CONSEGUIR CHEGAR",0
+	mInstrucoes3 BYTE "EM 90 SEGUNDOS.",0
+	
+	mInstrucoes4 BYTE "NESSA AVENTURA O ETEVALDO SO PODE PULAR SE A",0 
+	mInstrucoes5 BYTE "PLATAFORMA IMEDIATAMENTE ACIMA DELE ESTIVER COM",0 
+	mInstrucoes6 BYTE "UMA DAS CORES SELECIONADAS.",0
+	
+	mInstrucoes7 BYTE "PARA CADA PLATAFORMA EXISTIRA OITO POSSIVEIS",0
+	mInstrucoes8 BYTE "CORES, E NO TOTAL SERA SELECIONADA DUAS DESSAS CORES",0
+	mInstrucoes9 BYTE "QUE LIBERARAO A PASSAGEM DO ET PARA A PLATAFORMA ACIMA.",0
+	
+	mInstrucoes10 BYTE "ALEM DE RESPEITAR AS CORES, DEVE-SE TOMAR CUIDADO",0
+	mInstrucoes11 BYTE "PARA QUE NAO PULE ONDE EXISTE UM BURACO BRANCO. ESTE",0
+	mInstrucoes12 BYTE "LEVERA O ETEVALDO PARA OUTRA DIMENSAO, FAZENDO O",0
+	mInstrucoes13 BYTE "JOGADOR PERDER.",0
+	
+	mInstrucoes14 BYTE "PARA GUIAR O ET NESSE JOGO BASTA:",0			
+	mInstrucoes15 BYTE "-PRESSIONAR A SETA PARA CIMA PARA PULAR;",0			
+	mInstrucoes16 BYTE "-PRESSIONAR AS SETAS PARA A DIREITA OU ESQUERDA",0
+	mInstrucoes17 BYTE "PARA SE MOVIMENTAR LATERALMENTE;",0			
+	mInstrucoes18 BYTE "-TAMBEM E POSSIVEL DAR SAIR DO JOGO A QUALQUER",0
+	mInstrucoes19 BYTE "MOMENTO, PRESSIONANDO 'Q'.",0
+	
+	;Os dados seguintes salvos na memória tem por objetivo armazenar o texto a ser exibido nos crédito
+	mcreditos1 BYTE "PARA GUIAR O ET NESSE JOGO BASTA:",0			
+	mcreditos2 BYTE "-PRESSIONAR A SETA PARA CIMA PARA PULAR;",0			
+	mcreditos3 BYTE "-PRESSIONAR AS SETAS PARA A DIREITA OU ESQUERDA",0
+	mcreditos4 BYTE "PARA SE MOVIMENTAR LATERALMENTE;",0			
+	mcreditos5 BYTE "-TAMBEM E POSSIVEL DAR SAIR DO JOGO A QUALQUER",0
+	mcreditos6 BYTE "MOMENTO, PRESSIONANDO 'Q'.",0
+	
+	
 	
 .code
 LimpaTela PROC
+;Imprime uma seta na posição desejada
+;Recebe:	
+;
+;
+;Retorna:
 	mov eax, black+(black*16)
 	call SETTEXTCOLOR
 	
-	mov dl, 0								
+	mov dl, 0
 	mov dh, 0
 	call GOTOXY
 	
-	mov ecx, 16							;Trecho para impressão da primeira linha da matriz do jogo, imprime 41 "!"
+	movzx ecx, tMaxY							
 	
 LLP1:					
 	mov dl, 0
@@ -30,9 +74,9 @@ LLP1:
 	
 	push ecx
 	
-	mov ecx, 41
+	movzx ecx, tMaxX
 LLP2:
-	mov al, '.'
+	mov al, ' '
 	call WRITECHAR
 	loop LLP2
 	
@@ -49,6 +93,112 @@ LLP2:
 	ret
 LimpaTela ENDP
 
+ImpPerso PROC
+;Imprime o personagem na tela
+;     @
+;    /#\
+;    / \
+;Recebe: bl - Coordenada y (correspondente a coluna) que o personagem aparece, considere o centro do boneco (posição desejada do caracter '#')
+;        bh - Coordenada x (correspondente a linha) que o personagem aparece, considere o centro do boneco (posição desejada do caracter '#')
+;Retorna: Sem retorno
+	mov eax, lightGreen+(black*16)
+	call SETTEXTCOLOR
+	
+	mov dh, bh
+	mov dl, bl
+	sub dh, 1
+	call GOTOXY
+	
+	mov al, '@'
+	call WRITECHAR
+	
+	mov dl, bl
+	dec dl
+	mov dh, bh
+	call GOTOXY
+	
+	mov al, '/'
+	call WRITECHAR
+	mov al, '#'
+	call WRITECHAR
+	mov al, '\'
+	call WRITECHAR
+	
+	mov dh, bh
+	mov dl, bl
+	inc dh
+	dec dl
+	call GOTOXY
+	
+	mov al, '/'
+	call WRITECHAR
+	mov al, ' '
+	call WRITECHAR
+	mov al, '\'
+	call WRITECHAR
+	
+	mov eax, white+(black*16)
+	call SETTEXTCOLOR
+	
+	mov dl, 0								
+	mov dh, tMaxY
+	call GOTOXY
+	
+	ret
+ImpPerso ENDP
+
+
+delPerso PROC
+;Imprime uma seta na posição desejada
+;Recebe:	
+;
+;
+;Retorna:	
+	mov eax, black+(black*16)
+	call SETTEXTCOLOR
+	
+	mov dh, bh
+	mov dl, bl
+	sub dh, 1
+	call GOTOXY
+	
+	mov al, ' '
+	call WRITECHAR
+	
+	mov dl, bl
+	dec dl
+	mov dh, bh
+	call GOTOXY
+	
+	mov ecx, 3
+LDP1:
+	mov al, ' '
+	call WRITECHAR
+	loop LDP1
+	
+	mov dh, bh
+	mov dl, bl
+	inc dh
+	dec dl
+	call GOTOXY
+	
+	mov ecx, 3
+LDP2:
+	mov al, ' '
+	call WRITECHAR
+	loop LDP2
+	
+	mov eax, white+(black*16)
+	call SETTEXTCOLOR
+	
+	mov dl, 0								
+	mov dh, tMaxY
+	call GOTOXY
+	
+	ret
+delPerso ENDP
+
+
 PrintSeta PROC
 ;Imprime uma seta na posição desejada
 ;Recebe:	
@@ -59,11 +209,11 @@ PrintSeta PROC
 	
 	mov ax, 0
 	mov al, posSeta
-	mov dx, 3
+	movzx dx, distPlat
 	mul dx
-	add ax, 5
+	add ax, platInicial
 	
-	mov dl, 10								
+	mov dl, 20								
 	mov dh, al
 	call GOTOXY
 	
@@ -91,11 +241,11 @@ LPS2:
 	
 	mov ax, 0
 	mov al, posSeta
-	mov dx, 3
+	movzx dx, distPlat
 	mul dx
-	add ax, 5
+	add ax, platInicial
 	
-	mov dl, 10								
+	mov dl, 20								
 	mov dh, al
 	call GOTOXY
 	
@@ -108,12 +258,11 @@ LPS2:
 	call WRITECHAR
 	
 	mov dl, 0
-	mov dh, 16
+	mov dh, tMaxY
 	call GOTOXY
 	
 	ret
 PrintSeta ENDP	
-
 
 Bordas PROC
 ;Imprime uma seta na posição desejada
@@ -124,30 +273,31 @@ Bordas PROC
 	mov eax, red+(black*16)
 	call SETTEXTCOLOR
 	
-	mov ecx, 41							;Trecho para impressão da primeira linha da matriz do jogo, imprime 41 "!"
+	movzx ecx, tMaxX							;Trecho para impressão da primeira linha da matriz do jogo, imprime tMaxX "!"
 	mov al, '!'
 L1:
 	call WRITECHAR
 	loop L1
 
-	mov ecx, 16							;Trecho para impressão dos limites laterais do Jogo, imprime 14 '!' de cada lado do inicio e fim da barra impressa anteriormente
+	movzx ecx, tMaxY							;Trecho para impressão dos limites laterais do Jogo, imprime tMaxY '!' de cada lado do inicio e fim da barra impressa anteriormente
 	mov dh, 1
 L2:
 	mov dl, 0
-	call GOTOXY							;IRVINE GOTOXY - Seta o cursor na coordenada(dh, dh)
+	call GOTOXY								;IRVINE GOTOXY - Seta o cursor na coordenada(dh, dh)
 	call WRITECHAR
 
-	mov dl, 40
+	mov dl, tMaxX
+	dec dl
 	call GOTOXY
 	call WRITECHAR
 	inc dh
 	loop L2
 
 
-	mov dl, 0								;Trecho para a impressão dos marcadores de tempo e pontuação
-	mov dh, 16
+	mov dl, 0								
+	mov dh, tMaxY
 	call GOTOXY
-	mov ecx, 41
+	movzx ecx, tMaxX
 L3:
 	call WRITECHAR
 	loop L3
@@ -164,27 +314,26 @@ Plataformas PROC
 ;
 ;
 ;Retorna:	
-	mov dl, 1								;Trecho para imprimir as 4 plataformas existentes no jogo (loop aninhados)
-	mov dh, 4
-	call GOTOXY
 	mov eax, green							;IRVINE green - Seleção de cores pré definidas no IRVINE
 	call SETTEXTCOLOR
 	mov ecx, 4
-	mov ebx, 4
-L5:
+	mov ebx, 6
+LP1:
 	mov dl, 1
 	mov dh, bl
 	call GOTOXY
-	add bl, 3
+	add bl, distPlat
 	push ecx
-	mov ecx, 39
-L4:
+	movzx eax, tMaxX
+	sub eax, 2
+	mov ecx, eax
+LP2:
 	mov al, ':'
 	call WRITECHAR
-	loop L4
+	loop LP2
 
 	pop ecx
-	loop L5
+	loop LP1
 	
 	mov eax, white+(black*16)
 	call SETTEXTCOLOR
@@ -201,26 +350,29 @@ TelaInicio PROC
 	mov eax, white+(black*16)
 	call SETTEXTCOLOR
 
-	mov dl, 15								;Trecho para imprimir o nome do  jogo na tela principal
-	mov dh, 2
+	mov dl, 24								;Trecho para imprimir o nome do  jogo na tela principal
+	mov dh, 3
 	call GOTOXY
 	mov edx, OFFSET nome
 	call WRITESTRING
 	
-	mov dl, 17								;Trecho para imprimir a opção iniciar na tela principal
-	mov dh, 5
+	movzx eax, platInicial
+	mov dl, 26								;Trecho para imprimir a opção iniciar na tela principal
+	mov dh, al 
 	call GOTOXY
 	mov edx, OFFSET biniciar
 	call WRITESTRING
 	
-	mov dl, 16								;Trecho para imprimir a opção Como jogar na tela principal
-	mov dh, 8
+	add al, distPlat
+	mov dl, 25								;Trecho para imprimir a opção Como jogar na tela principal
+	mov dh, al
 	call GOTOXY
 	mov edx, OFFSET bcomoJogar
 	call WRITESTRING
 	
-	mov dl, 17								;Trecho para imprimir a opção creditos na tela principal
-	mov dh, 11
+	add al, distPlat
+	mov dl, 26								;Trecho para imprimir a opção creditos na tela principal
+	mov dh, al
 	call GOTOXY
 	mov edx, OFFSET bcreditos
 	call WRITESTRING
@@ -229,7 +381,7 @@ TelaInicio PROC
 	call SETTEXTCOLOR
 	
 	mov dl, 0
-	mov dh, 16
+	mov dh, tMaxY
 	call GOTOXY
 	
 	ret
@@ -259,21 +411,216 @@ TelaJogo PROC
 	ret
 TelaJogo ENDP
 
-TelaIntrucoes PROC
+
+
+TelaInstrucoes PROC
 ;Imprime uma seta na posição desejada
 ;Recebe:	
 ;
 ;
 ;Retorna:	
-	mov eax, white+(black*16)
+LTI3:
+	call LimpaTela
+	call Bordas
+
+	mov eax, green+(black*16)
 	call SETTEXTCOLOR
 
-	mov dl, 15								;Trecho para imprimir o nome do  jogo na tela principal
+	mov dl, 18
 	mov dh, 2
 	call GOTOXY
+	
 	mov edx, OFFSET nome
 	call WRITESTRING
-TelaIntrucoes ENDP
+	
+	mov al, '/'
+	call WRITECHAR
+	
+	mov edx, OFFSET bcomoJogar
+	call WRITESTRING
+	
+	mov eax, white+(black*16)
+	call SETTEXTCOLOR
+	
+	mov dl, 5
+	mov dh, 4
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes1
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 5
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes2
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 6
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes3
+	call WRITESTRING
+	
+	mov dl, 5
+	mov dh, 8
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes4
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 9
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes5
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 10
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes6
+	call WRITESTRING
+	
+	mov dl, 5
+	mov dh, 12
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes7
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 13
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes8
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 14
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes9
+	call WRITESTRING
+
+	mov dl, 5
+	mov dh, 16
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes10
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 17
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes11
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 18
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes12
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 19
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes13
+	call WRITESTRING
+	
+	movzx eax, tMaxX
+	sub eax, 5
+	mov dl, al	
+	movzx eax, tMaxY
+	sub eax, 2
+	mov dh, al
+	call GOTOXY
+	
+	mov eax, green+(black*16)
+	call SETTEXTCOLOR
+	
+	mov al, '-'
+	call WRITECHAR
+	mov al, '>'
+	call WRITECHAR
+	
+	mov dl, 0
+	mov dh, tMaxY
+	call GOTOXY
+	
+LTI1:
+    mov  eax,50
+    call Delay
+    call ReadKey
+    jz   LTI1
+	cmp  dx, 0027h
+	jne LTI1
+	
+	call LimpaTela
+	call Bordas
+	
+	mov eax, green+(black*16)
+	call SETTEXTCOLOR
+	
+	mov dl, 18
+	mov dh, 2
+	call GOTOXY
+	
+	mov edx, OFFSET nome
+	call WRITESTRING
+	
+	mov al, '/'
+	call WRITECHAR
+	
+	mov edx, OFFSET bcomoJogar
+	call WRITESTRING
+	
+	mov dl, 13
+	mov dh, 4
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes14
+	call WRITESTRING
+	
+	mov eax, white+(black*16)
+	call SETTEXTCOLOR
+	
+	mov dl, 5
+	mov dh, 8
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes15
+	call WRITESTRING
+	mov dl, 5
+	mov dh, 11
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes16
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 12
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes17
+	call WRITESTRING
+	mov dl, 5
+	mov dh, 15
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes18
+	call WRITESTRING
+	mov dl, 3
+	mov dh, 16
+	call GOTOXY
+	mov edx, OFFSET mInstrucoes19
+	call WRITESTRING
+	
+	mov dl, 3
+	movzx eax, tMaxY
+	sub eax, 2
+	mov dh, al
+	call GOTOXY
+	
+	mov eax, green+(black*16)
+	call SETTEXTCOLOR
+	
+	mov al, '<'
+	call WRITECHAR
+	mov al, '-'
+	call WRITECHAR
+	
+	mov dl, 0
+	mov dh, tMaxY
+	call GOTOXY
+
+LTI2:
+    mov  eax,50
+    call Delay
+    call ReadKey
+    jz   LTI2
+	cmp  dx, 0025h
+	je LTI3
+	
+	ret
+TelaInstrucoes ENDP
 	
 TempoTela PROC
 ;Imprime uma seta na posição desejada
@@ -289,7 +636,7 @@ TempoTela PROC
 	inc eax
 	mov time, al
 	mov dl, 0
-	mov dh, 16
+	mov dh, tMaxY
 	call GOTOXY
 	ret
 TempoTela ENDP
@@ -319,8 +666,8 @@ start:
 	call TelaInicio
 	mov posSeta, 0
 	
-	mov dl, 10								
-	mov dh, 5
+	mov dl, 20							
+	mov dh, BYTE PTR [platInicial]
 	call GOTOXY
 	
 	mov eax, white+(black*16)
@@ -332,7 +679,7 @@ start:
 	call WRITECHAR
 	
 	mov dl, 0
-	mov dh, 16
+	mov dh, tMaxY
 	call GOTOXY
 	
 AguardaTecla1:
@@ -361,46 +708,28 @@ jogo:
 	call ScoreTela
 	call TempoTela
 	
+	
+	mov bl, 15
+	mov bh, 15
+	call ImpPerso
+	
 	mov eax, 1000
 	call DELAY
 	call TempoTela
+	
+	mov bl, 15
+	mov bh, 15
+	call delPerso
+	
 	mov eax, 1000
 	call DELAY
 	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
-	mov eax, 1000
-	call DELAY
-	call TempoTela
+	
+	
 	jmp fim
 	
 instrucoes:
-	call LimpaTela	
-	call Bordas
-	call Plataformas
-	;call TelaInstrucoes
+	call TelaInstrucoes
 	call READCHAR
 	jmp start
 	
@@ -422,5 +751,4 @@ fim:
 
 exit
 main ENDP
-END main
 END main
